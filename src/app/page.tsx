515d0b3d1023528828e4935bac01182eade5aeda
kkwatemba@ke.ci.org <kkwatemba@ke.ci.org>
2024-09-06 11:08:11 +0300

    paypment
    setup stripe and products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductReel from "@/components/ProductReel";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
@@ -49,8 +50,11 @@ export default function Home() {
           </Button>
         </div>
       </div>
-      {/* TODO: List Products */}
-    </MaxWidthWrapper>
+   <ProductReel
+          query={{ sort: 'desc', limit: 4 }}
+          href='/products?sort=recent'
+          title='Brand new'
+        />    </MaxWidthWrapper>
 
       <section className='border-t border-gray-200 bg-gray-50'>
         <MaxWidthWrapper className='py-20'>
