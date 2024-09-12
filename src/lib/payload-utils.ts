6b3fe0e0e19e150c09522ddd9c5cfebff3b7c0a4
kkwatemba@ke.ci.org <kkwatemba@ke.ci.org>
2024-09-12 11:42:05 +0300

    src\lib\payload-utils.ts
    update

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,33 +1,24 @@
-import { User } from '../payload-types';
-import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
-import { NextRequest } from 'next/server';
+import { User } from '../payload-types'
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
+import { NextRequest } from 'next/server'
 
 export const getServerSideUser = async (
   cookies: NextRequest['cookies'] | ReadonlyRequestCookies
-): Promise<User | null> => {
-  const token = cookies.get('payload-token')?.value;
+) => {
+  const token = cookies.get('payload-token')?.value
 
-  if (!token) {
-    return null;
-  }
-
-  try {
-    const meRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`, {
+  const meRes = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
+    {
       headers: {
         Authorization: `JWT ${token}`,
       },
-    });
-
-    if (!meRes.ok) {
-      console.error(`Failed to fetch user data: ${meRes.status} ${meRes.statusText}`);
-      throw new Error('Failed to fetch user data');
     }
+  )
 
-    const { user } = (await meRes.json()) as { user: User | null };
-    return user;
-
-  } catch (error) {
-    console.error('Error fetching user:', error);
-    return null;
+  const { user } = (await meRes.json()) as {
+    user: User | null
   }
-};
+
+  return { user }
+}
