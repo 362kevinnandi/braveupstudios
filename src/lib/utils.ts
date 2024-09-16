6f237fa1e1f5354456d367c46187832a9098c9ef
kkwatemba@ke.ci.org <362kevin@gmail.com>
2024-09-16 18:16:12 +0300

    logo

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -30,7 +30,7 @@ export function constructMetadata({
   title = 'Braveupstudios - the marketplace for digital houseplans',
   description = 'Braveupstudios is an open-source marketplace for high-quality digital houseplans.',
   image = '/thumbnail.png',
-  icons = '/favicon.ico',
+  icons = '/logo.png',
   noIndex = false,
 }: {
   title?: string
