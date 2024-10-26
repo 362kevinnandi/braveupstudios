948f707f5a799794a8591e205dfc8dae7b5bb34c
kkwatemba@ke.ci.org <362kevin@gmail.com>
2024-10-26 08:36:59 +0300

    icon4

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = async() => {
 
               <div className="ml-4 flex lg:ml-0">
                 <Link href="/">
-                  <Icons.logo className="h-20 w-20" />Braveups
+                  <Icons.logo className="h-16 w-16" />
                 </Link>
               </div>
               <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
