bb058197d604b0df0ce0a3295777cde024c24aea
kkwatemba@ke.ci.org <362kevin@gmail.com>
2024-10-26 08:41:44 +0300

    icons14

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = async() => {
 
               <div className="ml-4 flex lg:ml-0">
                 <Link href="/">
-                  <Icons.logo className="h-16 w-16" />
+                  <Icons.logo className="h-14 w-14" />
                 </Link>
               </div>
               <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
