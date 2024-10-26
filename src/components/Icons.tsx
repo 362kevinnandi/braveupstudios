328e6b93e5dcde0e0c533a8f289a762c98858f75
kkwatemba@ke.ci.org <362kevin@gmail.com>
2024-10-26 16:41:09 +0300

    icon12

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -22,7 +22,7 @@ export const Icons = {
           fill="#87befb" opacity="1"></path>
         
         {/* Text "Braveupstudios" */}
-        <text x="256" y="480" fontSize="32" fontWeight="bold" fill="#222222" textAnchor="middle">
+        <text x="256" y="480" fontSize="132" fontWeight="bold" fill="#222222" textAnchor="middle">
           Braveupstudios
         </text>
       </g>
