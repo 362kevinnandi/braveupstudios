f41189d37560e4f357c28e73456336a8c40fc51e
kkwatemba@ke.ci.org <362kevin@gmail.com>
2024-10-26 17:51:38 +0300

    rect pro

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -70,12 +70,13 @@ const ProductReel = (props: ProductReelProps) => {
 
       <div className='relative'>
         <div className='mt-6 flex items-center w-full'>
-          <div className='w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8'>
+          <div className='w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-3 lg:grid-cols-4 lg:gap-x-8'>
             {map.map((product, i) => (
               <ProductListing
                 key={`product-${i}`}
                 product={product}
                 index={i}
+                className="aspect-[3/2] w-full" // Aspect ratio and full width for rectangular shape
               />
             ))}
           </div>
