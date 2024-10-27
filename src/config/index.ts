3e74686c7fadc794d7f3ce09da9fe07999401e2e
kkwatemba@ke.ci.org <362kevin@gmail.com>
2024-10-27 22:46:22 +0300

    more edits navb

--- src/config/index.ts
+++ src/config/index.ts
@@ -86,23 +86,23 @@ export const PRODUCT_CATEGORIES = [
    {
         label: 'Custom Design',
         value: 'custom_design',
-        featured: [
-            {
-                name: 'Favorite Icon Picks',
-                href: "/products?category=icons",
-                imageSrc: '/nav/icons/picks.jpg',
-            },
-            {
-                name: 'New Arrivals',
-                href: '/products?category=icons&sort=desc',
-                imageSrc: '/nav/icons/new.jpg',
-            },
-            {
-                name: 'Bestselling Icons',
-                href: '/products?category=icons',
-                imageSrc: '/nav/icons/bestsellers.jpg',
-            },
-        ],
+        //featured: [
+          //  {
+            //    name: 'Favorite Icon Picks',
+              //  href: "/products?category=icons",
+               // imageSrc: '/nav/icons/picks.jpg',
+            //},
+           // {
+              //  name: 'New Arrivals',
+              //  href: '/products?category=icons&sort=desc',
+              //  imageSrc: '/nav/icons/new.jpg',
+           // },
+            //{
+              //  name: 'Bestselling Icons',
+               // href: '/products?category=icons',
+               // imageSrc: '/nav/icons/bestsellers.jpg',
+           // },
+        //],
     },
 ];
 
